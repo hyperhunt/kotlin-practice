16acdbc2c221343283881eba56913c05a2cce80a
Tony Branch <tonybranch.org@yahoo.com>
2020-10-13 20:36:34 +0300

    Add Arrays with for, forEachIndexed.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,5 +4,23 @@ fun main() {
 
     val userKotlin = UserKotlin("test@kotlin.dev")
     println("Kotlin example: ${userKotlin.email}")
-    println()
+
+    // Array
+    val items: Array<Int> = arrayOf(5,7,12,4,0,13)
+//    val someArray = arrayOf("String")
+
+//    println(items.set(0, 45))
+//    println(items[0])
+//    println(items.get(5))
+//    println(items.size)
+//
+//    for (el in items) {
+//        print("${el}: ")
+//    }
+
+    items.forEach {
+        print(it)
+    }
+    println("")
+    items.forEachIndexed {index, el -> print ("${index}: $el ")}
 }
