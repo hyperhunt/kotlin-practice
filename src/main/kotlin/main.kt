e081e624c12528d3ab3a42a0a05b0d7e5328f6ad
Tony Branch <tonybranch.org@yahoo.com>
2020-10-13 21:39:03 +0300

    Add: List<Any>, Map<String, Any>.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,38 +1,46 @@
-fun main() {
-    val userJava = UserJava("tony@kotlin.dev")
-    println("Java example: ${userJava.email}")
-
-    val userKotlin = UserKotlin("test@kotlin.dev")
-    println("Kotlin example: ${userKotlin.email}")
-
-    // Array
-//    val items: Array<Int> = arrayOf(5,7,12,4,0,13)
-//    val someArray = arrayOf("String")
-
-//    println(items.set(0, 45))
-//    println(items[0])
-//    println(items.get(5))
-//    println(items.size)
+//fun main() {
+//    val userJava = UserJava("tony@kotlin.dev")
+//    println("Java example: ${userJava.email}")
 //
-//    for (el in items) {
-//        print("${el}: ")
+//    val userKotlin = UserKotlin("test@kotlin.dev")
+//    println("Kotlin example: ${userKotlin.email}")
+//
+//    // Array
+////    val items: Array<Int> = arrayOf(5,7,12,4,0,13)
+////    val someArray = arrayOf("String")
+//
+////    println(items.set(0, 45))
+////    println(items[0])
+////    println(items.get(5))
+////    println(items.size)
+////
+////    for (el in items) {
+////        print("${el}: ")
+////    }
+//
+////    items.forEach {
+////        print(it)
+////    }
+////    println("")
+////    items.forEachIndexed {index, el -> print ("${index}: $el ")}
+//
+////    val items = listOf(5,7,12,4,0,13)
+//    val items: MutableList<Int> = mutableListOf(5, 7, 12, 4, 0, 13)
+//
+//
+//    println(items.last())
+//    println(items.indexOf(13)) // Find 5 in first position [0]
+//
+//    val user = mapOf("name" to "Tony", "age" to 42, "isHasCar" to true).also {
+//        println(it)
 //    }
-
+//    user.forEach { (key, value) -> println("$key -> $value")}
+//
+//    printArray(items)
+//}
+//
+//fun printArray(items: List<Any>) {
 //    items.forEach {
-//        print(it)
+//        println(it)
 //    }
-//    println("")
-//    items.forEachIndexed {index, el -> print ("${index}: $el ")}
-
-//    val items = listOf(5,7,12,4,0,13)
-    val items: MutableList<Int> = mutableListOf(5, 7, 12, 4, 0, 13)
-
-
-    println(items.last())
-    println(items.indexOf(13)) // Find 5 in first position [0]
-
-    val user = mapOf("name" to "Tony", "age" to 42, "isHasCar" to true).also {
-        println(it)
-    }
-    user.forEach { (key, value) -> println("$key -> $value")}
-}
+//}
