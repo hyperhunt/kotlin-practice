b522f55f09a153a79d067a719faf1f3a8e1e96a2
Tony Branch <tonybranch.org@yahoo.com>
2020-10-17 14:08:48 +0300

    Add: Enum Class.

---
+++ src/main/Basic/UserJava.java
@@ -0,0 +1,11 @@
+public class UserJava {
+//    private final String email;
+//
+//    public UserJava(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+}
