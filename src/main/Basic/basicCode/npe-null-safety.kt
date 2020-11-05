95486020beb82330d33d918ae62e02400f0e078b
Tony Branch <tonybranch.org@yahoo.com>
2020-11-05 21:40:12 +0300

    toIntOrNull

--- src/main/Basic/basicCode/npe-null-safety.kt
+++ src/main/Basic/basicCode/npe-null-safety.kt
@@ -8,3 +8,13 @@ fun getSt(): String? {
 //    throw KotlinNullPointerException()
     return null
 }
+
+fun main() {
+    do {
+        val i: Int? = readLine()?.toIntOrNull()
+        when (i) {
+            in -300..300, in -1000..1000 -> println("$i in range.")
+            else -> println("$i out of range.")
+        }
+    } while (i != null)
+}
