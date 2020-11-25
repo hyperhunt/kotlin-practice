f83c3868d7693a0937d3ee52083afb1053ece423
Tony Branch <tonybranch.org@yahoo.com>
2020-11-25 19:22:22 +0300

    Basic HyperSkill.kt 25.11

---
+++ src/main/kotlin/HyperSkill.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+fun main() {
+
+//    val a: Byte = 100              //1
+//    val b: Byte = 127             //2
+//    val c: Byte = (100 + 100).toByte()        //3
+//    val d: Byte = (a + a).toByte()            //4
+//    val e: Byte = (a + a).toByte() //5
+
+//    val n: Int = readLine()?.toInt() ?: 0
+
+//    val i = 101
+//    val d = 3.1415
+//    val s = "32"
+//
+//    i.toDouble()
+//    d.toLong()
+//    d.toString()
+//    s.toInt()
+
+
+/*    val scanner = Scanner(System.`in`)
+    val s1 = scanner.nextLine()
+    val n = scanner.nextInt()
+    val s2 = scanner.next()
+
+//    println("scanner: $scanner")
+    println("s1: $s1")
+    println("n: $n")
+    println("s2: $s2")*/
+}
